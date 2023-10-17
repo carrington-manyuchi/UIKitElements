f7a8b0c6e02a323463129666f422f511c895c6ec
Carrington <Manyuchi>
2023-10-17 12:50:36 +0200

    Initial Commit

---
+++ UIKitElements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitElements
+//
+//  Created by DA MAC M1 157 on 2023/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
